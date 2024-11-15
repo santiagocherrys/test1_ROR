54642d089b42d27ab4bfb98a5c7ff5919b8e9e74
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-11-15 14:07:47 -0500

    Added the publication form partial but doesn't work :/

--- readers_social_media/app/controllers/publications_controller.rb
+++ readers_social_media/app/controllers/publications_controller.rb
@@ -12,10 +12,21 @@ class PublicationsController < ApplicationController
     @publication = Publication.new
   end
 
+  def create
+    @publication = Publication.new(publication_params)
+
+    if @publication.save
+      format.html { redirect_to @publication, notice: "Publication was successfully created." }
+      format.json { render :show, status: :created, location: @publication }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
+
   private
 
   def publication_params
-    params.require(:publication).permit(:title, :description, :category)
+    params.require(:publication).permit(:title, :description, :category, :user_id)
   end
   
 end
--- readers_social_media/config/routes.rb
+++ readers_social_media/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  #routes for publications
+  resources :publications
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #resources :publications
   #get '/', to: 'publications#index'
